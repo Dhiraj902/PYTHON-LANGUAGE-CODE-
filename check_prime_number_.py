b4af9d6c9bfb7c0ee176ae4e7fa36d543c303ef6
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-15 17:58:32 +0530

    Create check_prime_number_.py

---
+++ check_prime_number_.py
@@ -0,0 +1,39 @@
+#wap to check the number of prime or not prime. withaut using function ..
+num = int(input("Enter the  number:"))
+for i in range(2,num+1):
+    if(num%i==0):
+        break
+if(i==num):
+    print("The number is prime.",num)
+else:
+    print("The number is not prime:",num)
+
+(1)#wap to check the number of prime or not prime using function
+def prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num+1):
+        if num%i==0:
+            break
+    if(num==i):
+        return True
+    else:
+        return False
+print(prime(1))
+
+
+(2)#wap to check the number of prime or not prime using function
+def prime(num):
+    if num<=1:
+        print("The number is not prime:",num)
+        return  
+    for i  in range(2,num+1):
+        if(num % i == 0 ):
+            break
+    if(num == i):
+        print("the number is peime:",num)
+    else:
+        print("The number is not  prime:",num)
+        
+num = int(input("Enter the number for check prime or not prime:"))
+prime(num)
