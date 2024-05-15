589ee42e4e855bf3abed9af14aad28f5f5a16ab7
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-15 16:45:28 +0530

    Create add_two_number_.py

---
+++ add_two_number_.py
@@ -0,0 +1,11 @@
+#wap to print the sum of  two number without using function... 
+
+num1 = int(input("Enter the first number :")
+num2 = int(input("Enter the second number :")
+print(num1+num2) 
+
+#wap to print the sum of two number by using function...
+
+def sum(num1,num2):
+  return num1+num2
+sum(2,3)
