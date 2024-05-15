6ab9c6e312a774117c8b7365d30294fd687ed317
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-15 17:00:34 +0530

    Update add_two_number_.py

--- add_two_number_.py
+++ add_two_number_.py
@@ -1,7 +1,7 @@
 #wap to print the sum of  two number without using function... 
 
-num1 = int(input("Enter the first number :")
-num2 = int(input("Enter the second number :")
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
 print(num1+num2) 
 
 #wap to print the sum of two number by using function...
@@ -9,3 +9,10 @@ print(num1+num2)
 def sum(num1,num2):
   return num1+num2
 sum(2,3)
+
+
+def sum():
+  return num1+num2
+num1= int(input("Enter the first number :")
+num2 = int(input("Enter the second  number:")
+print(sum())
