1382a5dd10e654cffc39d7800afe83153b32b27f
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-16 22:25:34 +0530

    Create area_of_circle_.py

---
+++ area_of_circle_.py
@@ -0,0 +1,17 @@
+#wap to print the area of circle  without using function.
+ r = float(input("Enter the redies of circle..."))
+area = (22/7)*r**r
+print("The area of circle is = ",area)
+
+
+1.#wap to print the area of circle by using function.
+def area(r):
+    return (22/7)*r**r
+ print(area(2))
+
+
+2.#wap to print the area of circle by using function.
+def area():
+    return (22/7)*r**r
+r= float(input("Enter the redius of circle:"))
+print(area())
