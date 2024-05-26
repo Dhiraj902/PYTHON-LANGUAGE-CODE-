a1040c1bbd3c4690c5b9bad5c9d683029688e4f7
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-26 21:47:30 +0530

    Create area_of_triangle_.py

---
+++ area_of_triangle_.py
@@ -0,0 +1,42 @@
+#wap to find the area of triangle:
+L = float(input("Enter the Length:"))
+
+B = float(input("Enter the Breadth"))
+
+area = 1/2*L*B
+
+print(area)
+
+
+
+
+1.#wap to find the area of triangle using function:
+def area():
+    return 1/2*L*B
+
+L = float(input("Enter the Length:"))
+
+B = float(input("Enter the Breadth"))
+
+print(area())
+
+
+2.#wap to find the area of triangle using function:
+def area(L,B = 3):
+    
+  return 1/2*L*B
+
+print(area(2))
+
+
+
+3.#wap to find the area of triangle using function:
+def area():
+   
+  return 1/2*L*B
+
+L = float(input("Enter the Length:"))
+
+B = float(input("Enter the Breadth"))
+
+print(area())
