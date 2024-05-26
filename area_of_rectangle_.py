eb9272029051df175ba9b6a47089a1f90fdc01f8
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2024-05-26 21:42:55 +0530

    Create area_of_rectangle_.py

---
+++ area_of_rectangle_.py
@@ -0,0 +1,28 @@
+#wap to find the area of rectangle:
+L =float(input("Enter the value:"))
+
+W = float(input("Enter the value:"))
+
+area = L*W
+
+print(area) 
+
+
+1.#wap to find the area of rectangle using function:
+def area(L,W):
+    
+  return L*W
+
+print(area(2,2))
+
+
+2.#wap to find the area of rectangle using function:
+def area():
+    
+  return L*B
+
+L = float(input("Enter the value:"))
+
+B = float(input("Entr the value:"))
+
+print(area())
