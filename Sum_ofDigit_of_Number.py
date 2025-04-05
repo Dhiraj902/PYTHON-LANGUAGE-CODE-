2bfbcb6b9e2d8fda8a88b75f4af7f319129ac24f
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2025-04-05 23:25:17 +0530

    Sum_ofDigit_of_Number.py

---
+++ Sum_ofDigit_of_Number.py
@@ -0,0 +1,13 @@
+# Sum of digit Given the number
+# without Space
+
+
+def reverseNumber(n):
+    d=0
+    while(n!=0):
+        x=n%10
+        d =d*10+x
+        n= n//10
+    print(d)
+n = int(input("Enter the number here:"))
+reverseNumber(n)
