bba3deb4d9d5c12b9c6ded24673ba0d4b6e9b7b6
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2025-04-05 10:45:46 +0530

    max_value_permutation.py

---
+++ max_value_permutation.py
@@ -0,0 +1,11 @@
+# print Max Value Permutation
+
+def maxValue( arr): 
+    arr.sort()
+    A = 0
+    mod = 1000000007  
+    for i in range(len(arr)):
+        A += arr[i]*i
+    return A %mod
+arr = [21,32,6,596,7745,365]
+print(maxValue(arr))
