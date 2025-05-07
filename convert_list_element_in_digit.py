c05844970ed003dbb49981519af1ae9a74d70b66
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2025-05-07 10:14:56 +0530

    convert_list_element_in_digit.py

---
+++ convert_list_element_in_digit.py
@@ -0,0 +1,9 @@
+ # convert the List element into  the Digit 
+# like [1,2,3,4] = 1234
+
+def plush_one(nums):
+    for i in nums:
+        print(i,end='')
+    
+nums  = [2,3,4,4,5,6,7]
+plush_one(nums)
