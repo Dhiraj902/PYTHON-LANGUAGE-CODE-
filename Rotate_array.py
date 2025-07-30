09eb3d8be33cd8cfdff4f0e4fa2f6bc184af3316
DHEERAJ KUSHWAHA <165313867+Dhiraj902@users.noreply.github.com>
2025-07-31 01:27:08 +0530

    Rotate_array.py

---
+++ Rotate_array.py
@@ -0,0 +1,30 @@
+""" Rotate The array 
+Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
+
+Note: Consider the array as circular.
+
+Examples :
+
+Input: arr[] = [1, 2, 3, 4, 5], d = 2
+Output: [3, 4, 5, 1, 2]
+Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
+Input: arr[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], d = 3
+Output: [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+Explanation: when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
+Input: arr[] = [7, 3, 9, 1], d = 9
+Output: [3, 9, 1, 7]
+Explanation: when we rotate 9 times, we'll get 3 9 1 7 as resultant array.
+"""
+def rotateArr(arr, d):
+    A = len(arr)
+    d = d % A
+    return arr[d:] + arr[:d]
+
+li = []
+N = int(input("Enter the size of array: "))           
+for i in range(N):
+    S = int(input("Enter the number here: "))
+    li.append(S)
+
+d = int(input("Enter the number to rotate array: "))
+print(rotateArr(li, d))
